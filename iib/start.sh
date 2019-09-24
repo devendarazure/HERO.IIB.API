0e52098dc1ae0db6583bf6fac858fab94118eda3
Kumar Abhinav <development.kumarabhinav@gmail.com>
2019-09-24 10:58:31 +0000

    Updated start.sh

--- iib/start.sh
+++ iib/start.sh
@@ -12,8 +12,9 @@ cd  /home/iibuser/HERO.IIB.API
 
 ls -ltr
 
-cd
+touch testdevendar.txt
 
+ls -ltr
 #Xvfb :0 & DISPLAY=”:0” /opt/ibm/iib-10.0.0.11/tools/mqsicreatebar -data /home/iibuser/HERO.IIB.API -b myflow.bar -p TestIIBProject
 
 #DISPLAY=”:0” /opt/ibm/iib-10.0.0.11/tools/mqsicreatebar -data /home/iibuser/HERO.IIB.API -b /home/iibuser/HERO.IIB.API/myflow.bar -p TestIIBProject
