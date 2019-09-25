1184c8cf4a82d8802d34a1d4a8d75ced4c001a61
Kumar Abhinav <development.kumarabhinav@gmail.com>
2019-09-25 13:29:22 +0000

    Updated start.sh

--- iib/start.sh
+++ iib/start.sh
@@ -12,13 +12,13 @@ cd  /home/iibuser/HERO.IIB.API
 
 ls -ltr
 
-touch testdevendar.txt
+touch testdevendar25sept7PM.txt
 
 ls -ltr
 #Xvfb :0 & DISPLAY=”:0” /opt/ibm/iib-10.0.0.11/tools/mqsicreatebar -data /home/iibuser/HERO.IIB.API -b myflow.bar -p TestIIBProject
 
 #DISPLAY=”:0” /opt/ibm/iib-10.0.0.11/tools/mqsicreatebar -data /home/iibuser/HERO.IIB.API -b /home/iibuser/HERO.IIB.API/myflow.bar -p TestIIBProject
 
-sudo Xvfb :0 & DISPLAY=”:0” /opt/ibm/iib-10.0.0.11/tools/mqsicreatebar -data /home/iibuser/HERO.IIB.API -b myflow.bar -p TestIIBProject
+sudo Xvfb :0 & DISPLAY=”:0” /opt/ibm/iib-10.0.0.11/tools/mqsicreatebar -data /home/iibuser/HERO.IIB.API -b testbar25sept7PM.bar -P TestIIBProject -o /home/iibuser/HERO.IIB.API/TestIIBProject/Test_devops.msgflow/Test_devops.msgflow
 
 #sudo DISPLAY=”:0” /opt/ibm/iib-10.0.0.11/tools/mqsicreatebar -data /home/iibuser/HERO.IIB.API -b /home/iibuser/HERO.IIB.API/myflow.bar -p TestIIBProject
