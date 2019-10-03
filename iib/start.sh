bf95447077b98a41b4aaebdcd5b6b2e85ca13998
Kumar Abhinav <development.kumarabhinav@gmail.com>
2019-10-03 14:44:05 +0000

    Updated start.sh

--- iib/start.sh
+++ iib/start.sh
@@ -18,7 +18,7 @@ echo "***************BAR build command execution started here*******************
 
 #ls -ltr
 
-sudo mkdir /home/iibuser/HERO.IIB.API/buildout
+
 
 #Xvfb :0 & DISPLAY=”:0” /opt/ibm/iib-10.0.0.11/tools/mqsicreatebar -data /home/iibuser/HERO.IIB.API -b myflow.bar -p TestIIBProject
 
